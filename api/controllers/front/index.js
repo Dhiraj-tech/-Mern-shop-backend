862c93fe25a79deb000728d8a3ab08e84e76920d
Dhiraj-tech <dhirajrayofficial@gmail.com>
2023-12-21 11:15:52 +0545

    Final commit

--- api/controllers/front/index.js
+++
@@ -1,8 +0,0 @@
-import productsCtrl from "./products.controller.js";
-import listCtrl from "./list.controller.js";
-import checkoutCtrl from "./checkout.controller.js";
-import profileCtrl from "./profile.controller.js";
-
-export {productsCtrl, listCtrl, checkoutCtrl, profileCtrl}
-
-
